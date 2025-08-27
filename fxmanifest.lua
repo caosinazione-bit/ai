2e0886ed07eb24afc02f7777f7d77cc9a71bb276
caosinazione-bit <caosinazione@gmail.com>
2025-08-28 00:57:01 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'CaosInAzione + ChatGPT'
+description 'NPC Poliziotto AI con Google Studio AI (QBCore)'
+version '1.0.0'
+
+shared_script '@qb-core/shared.lua'
+
+client_script 'client.lua'
+server_script 'server.lua'
